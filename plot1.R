3cc39a4dc0337a7cca0d83b1499e8d41919ef44d
Kelly Kilgour <kelly.lynn.kilgour@gmail.com>
2014-08-10 16:45:39 -0400

    Plot 1 complete, Plot 2 framework

---
+++ plot1.R
@@ -0,0 +1,6 @@
+d <- read.csv('household_power_consumption.txt',sep=';',na.strings="?")
+d[,'Date'] <- as.Date(d[,'Date'], "%d/%m/%Y")
+d <- subset(d, Date==as.Date('2007-2-2') | Date==as.Date('2007-02-01'))
+png('plot1.png')
+hist(d[,'Global_active_power'], col="RED", main="Global Active Power", xlab="Global Active Power (kilowatts)", plot=TRUE)
+dev.off()
